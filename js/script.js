5c3b0991f76da0391adf66afdf0f7adade10d212
lauracimpoesu <lauracsbusiness@gmail.com>
2021-09-01 17:04:12 +0200

    my cool ToDo List :)

---
+++ js/script.js
@@ -0,0 +1,100 @@
+var tasksGroup = []; 
+var i; 
+var buttonOne; 
+var nameValue; 
+var tasksListHTML;
+var myContentDisplay;
+var myPara; 
+window.addEventListener("load", startProgram);
+
+function startProgram() {
+    buttonOne = document.querySelector('#myButtonOne'); 
+    nameValue = document.querySelector('#myNameValue'); 
+    tasksListHTML = document.querySelector("#myHTMLtasksList");
+    
+
+    //Local Storage p. I 
+/* nameValue.addEventListener('input', letter => {
+    tasksListHTML.textContent = letter.target.value; 
+})
+ */
+    buttonCall();
+}
+
+function buttonCall() {
+buttonOne.addEventListener('click', putTask, p);
+}
+
+function putTask() { 
+    tasksGroup.push(nameValue.value);
+    createDisplayMagic(); 
+    p(); 
+}  
+
+function deleteTask(id){
+    tasksGroup.pop(id, 1);
+    createDisplayMagic(); 
+}
+
+function createDisplayMagic() {
+        myContentDisplay = "";
+        for (   i=0; i < tasksGroup.length; i++   ) {
+            myContentDisplay += "<li class='list-group-item' id='text'>" + tasksGroup[i] + "<p onclick='deleteTask("+i+")' class='deleteButtonOne badge text-secondary'>undo</p></li>";
+        }
+        tasksListHTML.innerHTML = myContentDisplay; 
+    }
+
+    function p() {
+        for (   i=0; i < tasksGroup.length; i++   ) {
+        myPara = document.querySelector("#myPara"); 
+        myPara.innerText = tasksGroup.length;
+    } 
+}
+
+    //Local Storage p. II
+/* var saveToLocalStorage = () => {
+    localStorage.setItem('textinput', tasksListHTML.textContent) 
+}
+ */
+    
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
